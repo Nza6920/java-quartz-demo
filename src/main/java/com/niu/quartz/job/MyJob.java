35a9203a9c28bdf4a700942a9c52052e268bcaeb
Nza6920 <1484663282@qq.com>
2021-01-09 23:14:33 +0800

    防止 job 并发

--- src/main/java/com/niu/quartz/job/MyJob.java
+++ src/main/java/com/niu/quartz/job/MyJob.java
@@ -1,6 +1,7 @@
 package com.niu.quartz.job;
 
 import lombok.Data;
+import lombok.SneakyThrows;
 import org.quartz.*;
 
 import java.time.LocalTime;
@@ -11,17 +12,24 @@ import java.util.Calendar;
  *
  * 默认每次创建一个新的 Job 对象
  *
+ * '@DisallowConcurrentExecution' 禁止并发任务
+ *
+ *
  * @author [nza]
  * @version 1.0 2021/1/7
  * @createTime 21:30
  */
 @Data
+@DisallowConcurrentExecution
 public class MyJob implements Job {
 
     private String name;
 
     @Override
     public void execute(JobExecutionContext context) {
+
+        System.out.println("当前线程: " + Thread.currentThread().getName());
+        System.out.println(LocalTime.now());
 
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 
@@ -31,9 +39,17 @@ public class MyJob implements Job {
         System.out.printf("我是: %s", name);
         System.out.println();
 
-
         // 第二 种获取 jobData 的方法
-        System.out.printf("我是: %s", this.name);
-        System.out.println();
+//        System.out.printf("我是: %s", this.name);
+//        System.out.println();
+
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("任务执行完毕");
+            System.out.println("----------------------------------------------------");
+        }
     }
 }
